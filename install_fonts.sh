0ff86de9f9e2a12ada7f3c44ded86fd5ce40eac7
rafserqui <rafserqui@gmail.com>
2022-07-30 12:35:30 +0200

    Shallow clone

--- install_fonts.sh
+++ install_fonts.sh
@@ -16,7 +16,7 @@ wget https://github.com/be5invis/Iosevka/releases/download/v11.2.3/super-ttc-ios
 unzip super-ttc-iosevka-11.2.3.zip
 mv ./super-ttc-iosevka-11.2.3/sgr-iosevka-term.ttc ~/.fonts/
 
-git clone https://github.com/ryanoasis/nerd-fonts.git
+git clone https://github.com/ryanoasis/nerd-fonts.git --depth=1
 cd ./nerd-fonts
 ./install.sh
 
