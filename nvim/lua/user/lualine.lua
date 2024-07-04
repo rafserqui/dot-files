dfd80081e62f8314f12dc35b0955ed595310fadc
rafserqui <rafserqui@gmail.com>
2024-07-04 12:03:41 +0200

    Simplify lualine

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -1,12 +1,53 @@
 M = {}
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-	return
+    return
 end
 
 lualine.setup({
-	options = {
-		icons_enabled = true,
-		theme = "ayu",
-	},
+    options = {
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        theme = "ayu",
+        globalstatus = true,
+        disabled_filetypes = { statusline = { "dashboard" } },
+    },
+    sections = {
+        lualine_a = { { "mode", icon = " " } },
+        lualine_b = {
+            { "branch", icon = " " },
+            { "diff" },
+            {
+                "filename",
+                icon = " ",
+                path = 1
+            },
+        },
+        lualine_c = {
+            {
+                "diagnostics",
+                symbols = {
+                    error = " ",
+                    warn = " ",
+                    info = " ",
+                    hint = "󰝶 ",
+                },
+            },
+        },
+        lualine_x = {},
+        lualine_y = {
+            {
+                "location",
+                icon = "  "
+            },
+        },
+        lualine_z = {
+            {
+                "filetype",
+                icon_only = true,
+                separator = "",
+                padding = { left = 1, right = 0 }
+            },
+        }
+    }
 })
